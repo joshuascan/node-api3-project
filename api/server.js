c5d09ef2c1f97e4e6dd72937e79f26196f843260
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-07 13:28:29 -0700

    Creates scaffolding

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const usersRouter = require("./users/users-router");
+const usersRouter = require("./users/users-router");
 const { logger } = require("./middleware/middleware");
 
 const server = express();
@@ -8,7 +8,7 @@ server.use(express.json());
 
 server.use(logger);
 
-// server.use("/api/users", usersRouter);
+server.use("/api/users", usersRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,11 @@
 const express = require("express");
-// const Users = require("./users-model");
-// const Posts = require("../posts/posts-model");
+const Users = require("./users-model");
+const Posts = require("../posts/posts-model");
+const {
+  validateUserId,
+  validateUser,
+  validatePost,
+} = require("../middleware/middleware");
 
 const router = express.Router();
 
@@ -9,38 +14,45 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  res.status(200).json({ message: "GET successful" });
 });
 
 router.get("/:id", (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  res.status(200).json({ message: "GET successful" });
 });
 
 router.post("/", (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  res.status(200).json({ message: "POST successful" });
 });
 
 router.put("/:id", (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  res.status(200).json({ message: "PUT successful" });
 });
 
 router.delete("/:id", (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  res.status(200).json({ message: "DELETE successful" });
 });
 
 router.get("/:id/posts", (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  res.status(200).json({ message: "GET successful" });
 });
 
 router.post("/:id/posts", (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  res.status(200).json({ message: "POST successful" });
 });
 
 module.exports = router;
